c582e762a78e81045e954ccfcc4aedede49436c4
Tuomas Oikarinen <tuomas@mit.edu>
2019-12-04 18:44:52 -0500

    changed default learning rate

--- cgan_updated.py
+++ cgan_updated.py
@@ -24,7 +24,7 @@ from PIL import Image
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--lr", type=float, default=0.0001, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
