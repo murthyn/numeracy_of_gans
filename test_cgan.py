abb9d9e2f35b87c29b3a62f1527548c19ec6eda3
Nikhil Murthy <nmurthy@mit.edu>
2019-12-05 18:28:13 -0500

    remove print statements

--- test_cgan.py
+++ test_cgan.py
@@ -221,15 +221,9 @@ def inception_score(images, batch_size=5, epsilon=1e-20):
         print(s.shape)
         scores.append(s)
     p_yx = F.softmax(torch.cat(scores, 0), 1)
-    print("num nans p_yx", torch.sum(torch.isnan(p_yx)))
-    print("p_yx", p_yx)
     p_y = p_yx.mean(0).unsqueeze(0).expand(p_yx.size(0), -1)
-    print("num nans p_y", torch.sum(torch.isnan(p_y)))
-    print("p_y", p_y)
     KL_d = p_yx * (torch.log(p_yx + epsilon) - torch.log(p_y + epsilon))
-    print("num nans KL_d", torch.sum(torch.isnan(KL_d)))
     final_score = KL_d.mean()
-    print("final_score", final_score)
     return final_score
 
 def sample_images(numbers, times=1):
