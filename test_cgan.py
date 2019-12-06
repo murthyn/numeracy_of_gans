e92779ea47ec09c1ae99f7fbf7f02653b6bc1f5c
Nikhil Murthy <nmurthy@mit.edu>
2019-12-05 19:37:23 -0500

    fix cuda bug

--- test_cgan.py
+++ test_cgan.py
@@ -228,8 +228,7 @@ def inception_score(images, batch_size=5, epsilon=1e-20):
     return final_score
 
 def accuracy(images, labels, batch_size=5):
-    labels = torch.tensor(labels, dtype=torch.long)
-    if cuda: labels.cuda()
+    labels = torch.tensor(labels, dtype=torch.long, device = torch.device('cuda:0'))
     accuracies = []
     images = Variable(images.type(FloatTensor))
     for i in range(int(math.ceil(float(len(images)) / float(batch_size)))):
